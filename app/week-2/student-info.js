d0277703c4877879482c35c405c1128415b73a2f
gurleensingh01 <gurleengur03@gmail.com>
2024-09-12 14:13:04 -0600

    week 2 added

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>StudentInfo</h1>
+        <br></br>
+        <p>Student Name: Gurleen Singh</p>
+        <Link href="https://github.com/gurleensingh01/cprg306-assignments">Github Repository Link</Link>
+      </main>
+    );
+  }
